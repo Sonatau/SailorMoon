aeea9002e8c0847b3c1e0da47dcd006c88c953cb
lizzy507 <w929829853@163.com>
2021-04-16 17:49:41 +0800

    注册接口功能实现

--- app-cloud/daoyun/src/app/pages/passport/register/register.page.ts
+++ app-cloud/daoyun/src/app/pages/passport/register/register.page.ts
@@ -14,17 +14,18 @@ export class RegisterPage implements OnInit {
 
   public register_email: string = '';
   public register_password: string = '';
-  public verify_password: string = '';
   public register_name: string = '';
   public register_no: string = '';
+  public verify_password: string = '';
   public verify_code: string = '';
-  public return_code: any;  //1: 发送成功   -1: 发送失败
+  public return_code = -1;  //1: 发送成功   -1: 发送失败
+  public roleID: any; //1: 教师    2: 学生    0: 非法数据
+
   verifyCode: any = {
     verifyCodeTips: "获取验证码",
     countdown: 60,
     disable: true
   }
-  public role: any; //1: 教师    2: 学生
 
   constructor(public httpService: HttpService,
     public http: HttpClient,
@@ -75,99 +76,97 @@ export class RegisterPage implements OnInit {
   //----------------------------------------------------------------------------------//
   //----------------------------------------------------------------------------------//
 
+  //----------------------------------------------------------------------------------//
+  //-----------------------------------提交注册信息------------------------------------//
+  //----------------------------------------------------------------------------------//
   async onRegister(form: NgForm) {
     const loading = await this.loadingController.create({
       message: '请稍等...',
     });
     await loading.present();
-    if (this.hasCode()) {
-      if (form.valid) {
-        //验证验证码是否正确
-        // await this.onSendSMS();
-        if (this.verify_code == this.return_code) {
-          //两次密码输入是否一致
-          if (this.register_password == this.verify_password) {
-            var params = {
-              email: this.register_email,
-              password: this.register_password
-            }
-            if (this.role == '3') {
-              params["role_id"] = '3';
-            }
-            console.log(params);
-            var api = '/register';//后台接口
-            this.httpService.post(api, params).then(async (response: any) => {
-              await loading.dismiss();
-              if (response.data.respCode == 1) {//注册成功
-                let alert = await this.alertController.create({
-                  header: '提示',
-                  message: '注册成功！',
-                  buttons: [
-                    {
-                      text: '确认',
-                      cssClass: 'secondary',
-                      handler: (blah) => {
-                        this.router.navigateByUrl('/lesson-tabs');
-                        localStorage.setItem("email", this.register_email);
-                        localStorage.setItem("isLogin", "1");
-                        this.getInf(this.register_email);
-                      }
-                    }
-                  ]
-                });
-                alert.present();
-
-              } else if (response.data.respCode == '该邮箱已经注册过了！') {
-                let alert = await this.alertController.create({
-                  header: '提示',
-                  message: '该邮箱已经注册过了！',
-                  buttons: ['确定']
-                });
-                alert.present();
-              }
-            })
-
-          } else {
-            await loading.dismiss();
-            let toast = await this.toastController.create({
-              message: '两次密码不一致！',
-              duration: 2000
-            });
-            toast.present();
-          }
-        } else {
+    if(form.invalid){ //检验输入信息是否有效
+      await loading.dismiss();
+      let toast = await this.toastController.create({
+        message: '请输入有效信息！',
+        duration: 2000
+      });
+      toast.present();
+    }else{
+      if(this.return_code == -1){ //检验是否成功发送验证码
+        await loading.dismiss();
+        let toast = await this.toastController.create({
+          message: '请先获取验证码！',
+          duration: 2000
+        });
+        toast.present();
+      }else{
+        if (this.register_password != this.verify_password){
           await loading.dismiss();
           let toast = await this.toastController.create({
-            message: '验证码不正确！',
+            message: '两次密码不一致！',
             duration: 2000
           });
           toast.present();
+        }else{
+          var api = '/register';
+          var params = {
+            email: this.register_email,
+            password: this.register_password,
+            name: this.register_name,
+            sno: this.register_no,
+            mailVerificationCode: this.verify_code,
+            roleId: this.roleID
+          }
+          //console.log(params);
+          this.httpService.post(api, params).then(async (response: any) => {
+            await loading.dismiss();
+            if(response.data.respCode == -1){
+              let alert = await this.alertController.create({
+                header: '提示',
+                message: response.data.msg,
+                buttons: ['确定']
+              });
+              alert.present();
+            }else if(response.data.respCode == 1){
+              let alert = await this.alertController.create({
+                header: '提示',
+                message: '注册成功！',
+                buttons: [{
+                  text: '确认',
+                  cssClass: 'primary',
+                        handler: (blah) => {
+                          this.router.navigateByUrl('/login');
+                        }
+                }]
+              });
+              alert.present();
+            }
+          });
         }
       }
     }
   }
 
-  //获取个人信息
-  getInf(email) {
-    var params = {//后台所需参数
-      email: email,
-    };
-    var api = '/user/info';//后台接口
-    this.httpService.get(api, params).then(async (response: any) => {
-      if (response.status == 200) {
-        console.log(response.data.role)//````````````````````````````````````
-        localStorage.setItem("role", response.data.role);//```````````````````````````````````
-      }
-    })
-  }
 
-  //---------------可能没啥用标记着准备删掉---------------//
-  hasCode() {
-    if (this.verify_code != null && this.return_code != null) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-}
+  //---------------没啥用标记着准备删掉---------------//
+  // hasCode() {
+  //   if (this.verify_code != null && this.return_code != null) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  //   //获取个人信息
+  //   getInf(email) {
+  //     var params = {//后台所需参数
+  //       email: email,
+  //     };
+  //     var api = '/user/info';//后台接口
+  //     this.httpService.get(api, params).then(async (response: any) => {
+  //       if (response.status == 200) {
+  //         console.log(response.data.role)//````````````````````````````````````
+  //         localStorage.setItem("role", response.data.role);//```````````````````````````````````
+  //       }
+  //     })
+  //   }
+  // }
--- app-cloud/daoyun/src/app/shared/services/http.service.ts
+++ app-cloud/daoyun/src/app/shared/services/http.service.ts
@@ -16,16 +16,16 @@ export class HttpService {
   get(api, params) {
     return new Promise((resolve, reject) => {
       this.setToken(); 
-      axios.interceptors.request.use((config) => {
-        if (localStorage.getItem("token")) {
-          config.headers['token']=localStorage.getItem("token");
-        }
-        // console.log(this.commonUrl);
-        return config;
-      },(error) =>{
-        console.log('错误参数')
-        return Promise.reject(error);
-      });
+      // axios.interceptors.request.use((config) => {
+      //   if (localStorage.getItem("token")) {
+      //     config.headers['token']=localStorage.getItem("token");
+      //   }
+      //   // console.log(this.commonUrl);
+      //   return config;
+      // },(error) =>{
+      //   console.log('错误参数')
+      //   return Promise.reject(error);
+      // });
       axios.get(this.commonUrl + api, {
         params: params,
       }).then(function (response) {
